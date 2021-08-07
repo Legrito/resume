c90adb3353049784acb3f68ee5482fbfd7795161
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2021-08-07 18:50:42 +0300

    Edit resume

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const projectBtnEl = document.querySelector('#projects');
-const resumeBtnEl = document.querySelector('#resume');
-const projectsEl = document.querySelector('div.projects'); 
-const resumeEl = document.querySelector('div.resume');
-const greetingEl = document.querySelector('.greeting')
+const projectBtnEl = document.querySelector("#projects");
+const resumeBtnEl = document.querySelector("#resume");
+const projectsEl = document.querySelector("div.projects");
+const resumeEl = document.querySelector("div.resume");
+const greetingEl = document.querySelector(".greeting");
 
 projectBtnEl.active = false;
 resumeBtnEl.active = false;
 
-resumeBtnEl.addEventListener('click', () => { 
-  resumeEl.classList.toggle('js-block');
-  projectBtnEl.classList.toggle('active-btn');
-  resumeBtnEl.classList.toggle('active-btn');
-  if(projectsEl.classList.contains('js-block')) {
-    projectsEl.classList.remove('js-block');
-  }
-
-});
-
-projectBtnEl.addEventListener('click', () => {
-  projectsEl.classList.toggle('js-block');
-  projectBtnEl.classList.toggle('active-btn');
-  resumeBtnEl.classList.toggle('active-btn');
-  if(resumeEl.classList.contains('js-block')) {
-    resumeEl.classList.remove('js-block');
+resumeBtnEl.addEventListener("click", () => {
+  resumeEl.classList.toggle("js-block");
+  projectBtnEl.classList.toggle("active-btn");
+  resumeBtnEl.classList.toggle("active-btn");
+  if (projectsEl.classList.contains("js-block")) {
+    projectsEl.classList.remove("js-block");
   }
 });
 
+projectBtnEl.addEventListener("click", () => {
+  projectsEl.classList.toggle("js-block");
+  projectBtnEl.classList.toggle("active-btn");
+  resumeBtnEl.classList.toggle("active-btn");
+  if (resumeEl.classList.contains("js-block")) {
+    resumeEl.classList.remove("js-block");
+  }
+});
 
+let value = [1, 2];
+
+console.log(Object.__proto__);
